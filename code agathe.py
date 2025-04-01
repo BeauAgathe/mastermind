a988c968d31dbb6fea705b63712466ddeaece7f7
Agathe BEAU <agathe_beau@icloud.com>
2025-04-01 16:50:43 +0200

    past_games

--- code agathe.py
+++ code agathe.py
@@ -18,11 +18,25 @@ def sauvegarder(nom_partie):
     dico["résulat"] = r
     dico["nombre d'essais"] = current_canva
     dico["tentatives"] = tentatives
-    sauv.write(str(dico))
+    sauv.write(str(dico) + "\n")
     sauv.close()
 
 
 def voir_vieilles_parties():
     '''pour regarder les infos des parties enregistrées'''
     fichier = open('fichier de sauvegarde', "r")
+    past_games = []
+    a = fichier.readline()
+    while a != "":
+        past_games.append(a)
+        a = fichier.readline()
     fichier.close()
+    affiche_texte(past_games)
+
+
+def voir_vieilles_parties2():
+    '''pour regarder les infos des parties enregistrées'''
+    fichier = open('fichier de sauvegarde', "r")
+    past_games = fichier.readlines()
+    fichier.close()
+    affiche_texte(past_games["parties"])
